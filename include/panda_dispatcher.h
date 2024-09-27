9616447008aeba816faef50e1eb4ddcb7f90ea56
Raees Khan <rak177@pitt.edu>
2024-09-27 15:57:46 +0200

    map -> unordered_map

--- include/panda_dispatcher.h
+++ include/panda_dispatcher.h
@@ -25,8 +25,8 @@ namespace sg4 = simgrid::s4u;
 struct Job {
   std::string                       id{};
   int                               flops{};
-  std::map<std::string, size_t>     input_files{};
-  std::map<std::string, size_t>     output_files{};
+  std::unordered_map<std::string, size_t>     input_files{};
+  std::unordered_map<std::string, size_t>     output_files{};
   int                               cores{};
   std::string                       disk{};
   std::string                       comp_host{};
@@ -84,17 +84,17 @@ protected:
   void create_actors(const std::set<Host*>& hosts_with_jobs);
   void update_all_disks_content(const std::set<Host*>& hosts_with_jobs);
   void update_disk_content(sg4::Disk* d, const std::string& content);
-  std::map<std::string, size_t> merge_content(const std::vector<std::map<std::string, size_t>>& maps);
-  std::string get_disk_content(const std::map<std::string, size_t>& inputMap);
+  std::unordered_map<std::string, size_t> merge_content(const std::vector<std::unordered_map<std::string, size_t>>& maps);
+  std::string get_disk_content(const std::unordered_map<std::string, size_t>& inputMap);
   void getHostsINFO(sg4::NetZone* platform, std::vector<Site*>& sites);
   void cleanup(std::vector<Site*>& sites);
   void h5init();
   //Functions needed to specify hosts for jobs
-  double calculateWeightedScore(Host* cpu, Job* j, const std::map<std::string, double>& weights, std::string& best_disk_name);
-  double getTotalSize(const std::map<std::string, size_t>& files);
-  Host* findBestAvailableCPU(std::vector<Host*>& cpus, Job* j, const std::map<std::string, double>& weights);
+  double calculateWeightedScore(Host* cpu, Job* j, const std::unordered_map<std::string, double>& weights, std::string& best_disk_name);
+  double getTotalSize(const std::unordered_map<std::string, size_t>& files);
+  Host* findBestAvailableCPU(std::vector<Host*>& cpus, Job* j, const std::unordered_map<std::string, double>& weights);
   std::vector<Job*> splitTaskIntoJobs(Task& task, size_t& max_flops_per_job, size_t& max_storage_per_job);
-  void allocateResourcesToJobs(std::vector<Site*>& sites, TaskQueue& tasks, const std::map<std::string, double>& weights, size_t& max_flops_per_job,  size_t& max_storage_per_job, std::set<Host*>&   hosts_with_jobs);
+  void allocateResourcesToJobs(std::vector<Site*>& sites, TaskQueue& tasks, const std::unordered_map<std::string, double>& weights, size_t& max_flops_per_job,  size_t& max_storage_per_job, std::set<Host*>&   hosts_with_jobs);
   void printJobInfo(Job* job);  
   
 private:
@@ -106,7 +106,7 @@ private:
 
 
 
-  const std::map<std::string, double> weights = {
+  const std::unordered_map<std::string, double> weights = {
         {"speed", 1.0},
         {"cores", 1.0},
 	{"disk", 1.0},
--- include/platform.h
+++ include/platform.h
@@ -37,19 +37,19 @@ static sg4::NetZone* create_platform(const std::string& platform_name);
  *
  * This function creates one site made of nb_cpu CPUs.
  */
-static sg4::NetZone* create_site(sg4::NetZone* platform, const std::string& site_name, std::map<std::string, CPUInfo>& cpuInfo);
+static sg4::NetZone* create_site(sg4::NetZone* platform, const std::string& site_name, std::unordered_map<std::string, CPUInfo>& cpuInfo);
 
 /**
  *
  * This function creates a number of sites.
  */
-  std::map<std::string, sg4::NetZone*>  create_sites(sg4::NetZone* platform, std::map<std::string, std::map<std::string, CPUInfo>>& siteNameCPUInfo);;
+  std::unordered_map<std::string, sg4::NetZone*>  create_sites(sg4::NetZone* platform, std::unordered_map<std::string, std::unordered_map<std::string, CPUInfo>>& siteNameCPUInfo);;
 
 /**
  *
  * This function initializes the connection between various sites in a platform.
  */
-void initialize_site_connections(sg4::NetZone* platform, std::map<std::string, std::pair<double, double>>& siteConnInfo, std::map<std::string, sg4::NetZone*>& sites);
+void initialize_site_connections(sg4::NetZone* platform, std::unordered_map<std::string, std::pair<double, double>>& siteConnInfo, std::unordered_map<std::string, sg4::NetZone*>& sites);
 
 
 /**                                                                                                        
