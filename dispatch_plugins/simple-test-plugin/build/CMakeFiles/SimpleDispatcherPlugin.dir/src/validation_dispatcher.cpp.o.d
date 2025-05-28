d207bc7df8e56886065d130612874a6e4bcd0f42
Raees Khan <rak177@pitt.edu>
2025-05-28 16:42:53 +0200

    fixing cmake issues, cleaning up a bit as well

--- dispatch_plugins/simple-test-plugin/build/CMakeFiles/SimpleDispatcherPlugin.dir/src/validation_dispatcher.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/SimpleDispatcherPlugin.dir/src/validation_dispatcher.cpp.o: \
- /home/sairam/ATLASGRIDV2/ATLAS-GRID-SIMULATION/dispatch_plugins/simple-test-plugin/src/validation_dispatcher.cpp \
- /usr/include/stdc-predef.h
